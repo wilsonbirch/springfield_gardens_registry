79705ec0c52085ce83cda2ee2ecb16732948891c
Wilson Birch <wilsonbirch@gmail.com>
2023-09-25 15:53:45 -0400

    completed navbar framework, added foundation css

--- app/src/App.js
+++ app/src/App.js
@@ -1,53 +1,9 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-          <hr />
-
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
+      <Navbar />
     </div>
   );
 }
---
+++ app/src/components/navbar.js
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
+
+function Navbar() {
+  const [path, setPath] = useState('/');
+  const navItems = [
+    {
+      title: 'Home',
+      path: '/',
+    },
+    {
+      title: 'About',
+      path: '/about',
+    },
+    {
+      title: 'Dashboard',
+      path: '/dashboard',
+    },
+  ];
+
+  return (
+    <div>
+      {/* <div
+        class="title-bar"
+        data-responsive-toggle="springfield-gardens-registry-nav"
+        data-hide-for="small"
+      >
+        <button
+          class="menu-icon"
+          type="button"
+          data-toggle="springfield-gardens-registry-nav"
+        ></button>
+        <div class="title-bar-title">
+          Springfield Gardens Registry
+        </div>
+      </div> */}
+
+      <div
+        className="top-bar"
+        id="springfield-gardens-registry-nav"
+      >
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              Springfield Gardens Registry
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="dropdown menu" data-dropdown-menu>
+            <Router>
+              <div className="columns">
+                <ul className="menu">
+                  {navItems.map((item) => {
+                    return (
+                      <button
+                        onClick={() => setPath(item.path)}
+                      >
+                        <Link
+                          className={`clear button${
+                            path === item.path
+                              ? ''
+                              : ' secondary'
+                          }`}
+                          to={item.path}
+                        >
+                          {item.title}
+                        </Link>
+                      </button>
+                    );
+                  })}
+                </ul>
+              </div>
+            </Router>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
