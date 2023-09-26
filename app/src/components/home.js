1fd944eecc5dacbe9be7cd42e28a0bc78f66a871
Wilson Birch <wilsonbirch@gmail.com>
2023-09-26 00:08:17 -0400

    added advancedmarker script and converted csv to .json format and map through to add the markers to the map
    updated center location and zoom to fit all garden locations

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -1,11 +1,38 @@
+import { useEffect } from 'react';
+
+import community_gardens from './community_gardens.json';
+
 function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'js/advancedMarker.js';
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="home-container">
       <gmp-map
-        center="37.4220656,-122.0840897"
-        zoom="10"
-        map-id="DEMO_MAP_ID"
-      ></gmp-map>
+        id="marker-click-event-example"
+        center="45.32248682538945,-75.69433980392594"
+        zoom="11"
+        map-id="springfield-gardens-registry"
+      >
+        {community_gardens.map((garden, i) => {
+          return (
+            <gmp-advanced-marker
+              key={i}
+              position={`${garden.Y},${garden.X}`}
+              title={garden.GARDEN}
+            ></gmp-advanced-marker>
+          );
+        })}
+      </gmp-map>
     </div>
   );
 }
